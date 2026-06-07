0071bd40d037fdab45d0cba03ed403a6d33018d7
Cheryl Buzicky <152465899+cbuzicky@users.noreply.github.com>
2026-06-06 20:48:35 -0500

    Add files via upload

---
+++ Enhancement_2/java/InventoryItem.java
@@ -0,0 +1,35 @@
+package com.zybooks.c499_buzicky_cheryl;
+
+public class InventoryItem {
+    private int id;
+
+    private String name;
+    private String sku;
+    private int quantity;
+
+    private String imageUrl;
+
+
+    public InventoryItem(int id, String name, String sku, int quantity, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.sku = sku;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getter methods
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public String getSku() { return sku; }
+    public int getQuantity() { return quantity; }
+
+    public String getImageUrl() {return imageUrl; }
+
+
+    // Setter methods
+    public void setName(String name) { this.name = name; }
+
+}
+
